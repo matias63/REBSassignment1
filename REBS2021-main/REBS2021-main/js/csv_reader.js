0535ad5b06767aea5ff5fdb86c41dcc5c1028a1a
MUBRBR <rlw149@alumni.ku.dk>
2023-12-19 09:50:19 +0100

    Made csv_reader more flexible in terms of filepath across different computers

--- REBS2021-main/REBS2021-main/js/csv_reader.js
+++ REBS2021-main/REBS2021-main/js/csv_reader.js
@@ -1,31 +1,40 @@
 const fs = require('fs');
+const path = require('path');
 
-function readCSV(filePath) {
-  // Read the CSV file
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+function readCSV(fileName) {
+    // Define the base path
+    const basePath = 'C:\\Users\\';
 
-    // Parse CSV content
-    const rows = data.split('\n');
-    const csvData = rows.map(row => row.split(','));
+    // Combine the base path with the dynamic part (fileName)
+    const filePath = path.join(basePath, fileName);
 
-    // Extract the first and third columns
-    const extractedData = csvData.slice(1).map(row => [row[0], row[2]]);
+    // Read the CSV file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+        console.error(err);
+        return;
+        }
 
-    console.log(extractedData);
-    // Do something with the extracted data
-    // Additional actions with extractedData can be added here
-    // For example, you could save the data to a file, process it further, etc.
-    // For demonstration, let's write it to a JSON file:
-    fs.writeFileSync('output.json', JSON.stringify(extractedData, null, 2), 'utf8');
+        // Parse CSV content
+        const rows = data.split('\n');
+        const csvData = rows.map(row => row.split(','));
+
+        // Extract the first and third columns
+        const extractedData = csvData.slice(1).map(row => [row[0], row[2]]);
+
+        console.log(extractedData);
+        // Do something with the extracted data
+        // Additional actions with extractedData can be added here
+        // For example, you could save the data to a file, process it further, etc.
+        // For demonstration, let's write it to a JSON file:
+        fs.writeFileSync('output.json', JSON.stringify(extractedData, null, 2), 'utf8');
   });
 }
 
-// Specify the path to your CSV file
-const filePath = 'C:\\Users\\Mads-\\OneDrive - University of Copenhagen\\Uni\\2023\\REB\\REBSassignment1\\REBS2021-main\\REBS2021-main\\log.csv';
-// console.log(filePath)
-// Call the readCSV function with the file path
-readCSV(filePath);
+// Specify the dynamic part of the file path
+const fileNameComputer1 = 'Mads-\\OneDrive - University of Copenhagen\\Uni\\2023\\REB\\REBSassignment1\\REBS2021-main\\REBS2021-main\\log.csv';
+// const fileNameComputer2 = 'Enter\\REB\\REBSassignment1\\REBS2021-main\\REBS2021-main\\log.csv';
+
+// Call the readCSV function with the file name
+readCSV(fileNameComputer1);
+// readCSV(fileNameComputer2);
